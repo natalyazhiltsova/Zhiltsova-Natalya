886f555f85b99eacf2c518c9b3b4b9a383a58ace
natalyazhiltsova <71863393+natalyazhiltsova@users.noreply.github.com>
2020-10-02 16:06:19 +0700

    Update Lecture02.py

--- Lecture02/Lecture02.py
+++ Lecture02/Lecture02.py
@@ -1,3 +1,45 @@
 a=int(input("Введите число от 0 до 99: "))
 if a > 99: 
   print ("Некорректный ввод")
+elif a==0:
+  print("ноль")
+elif a==1:
+  print("один")
+elif a==2:
+  print("два")
+elif a==3:
+  print("три")
+elif a==4:
+  print("четыре")
+elif a==5:
+  print("пять")
+elif a==6:
+  print("шесть")
+elif a==7:
+  print("семь")
+elif a==8:
+  print("восемь")
+elif a==9:
+  print("девять")
+else a==10:
+  print("десять")
+elif a==11:
+  print("одиннадцать")
+elif a==12:
+  print("двенадцать")
+elif a==13:
+  print("тринадцать")
+elif a==14:
+  print("четырнадцать")
+elif a==15:
+  print("пятнадцать")
+elif a==16:
+  print("шестадцать")
+elif a==17:
+  print("семнадцать")
+elif a==18:
+  print("восемнадцать")
+elif a==19:
+  print("девятнадцать")
+else a==10:
+print("двадцать")
